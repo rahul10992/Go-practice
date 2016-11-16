591887025841afb31cc635f47d8d0483b76baf26
rahul10992 <rahul10992@gmail.com>
2016-11-15 18:04:44 -0800

    code cleanup
    removed warnings due to names

--- NewtonsMethodSquareroot/sqrt.go
+++ NewtonsMethodSquareroot/sqrt.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 	no := 1087.0
+	res := sqrt(no)
 	fmt.Println(res)
 
+	fmt.Println("Difference: ", makePositive(res-math.Sqrt(no)))
 }
 
+func makePositive(x float64) float64 {
 	if x >= 0 {
 		return x
 	}
@@ -18,16 +21,21 @@ func main() {
 }
 
 //Get square root using newton's method zn+1 = (zn - (((zn^2)-x)/(2*zn)))
+func sqrt(x float64) float64 {
 	z := 2.0
 	for i := 0; i < 10; i++ {
+		z = computeValue(z, x)
 	}
 	return z
 }
 
+func computeValue(zn float64, x float64) (zn1 float64) {
+	num := numerator(zn, x)
 	den := (2 * zn)
 	zn1 = zn - (num / den)
 	return
 }
 
+func numerator(zn float64, x float64) float64 {
 	return (zn * zn) - x
 }
