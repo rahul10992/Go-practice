4aca353a65f254e8158d0b94b388b242ae42c5bf
rahul10992 <rahul10992@gmail.com>
2016-11-16 18:09:18 -0800

    added that super difficult exercise that involves iterating a 2d slice
    this ends up printing a weird string of characters more than an image

---
+++ pic/pic.go
@@ -0,0 +1,35 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 { // is printing a bunch of characters. Looks more like a authentication bearer token than anything else.. need to know why.
+	var result = make([][]uint8, dx)
+	for i := 0; i < dx; i++ {
+		var k uint8 = 1
+		result[i] = make([]uint8, dy)
+		for j := 0; j < dy; j++ {
+			var l uint8 = 1
+			result[i][j] = average(k, l)
+			l++
+		}
+		k++
+	}
+	return result
+
+}
+
+func average(x, y uint8) uint8 {
+	return (x + y) / 2.0
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
+func power(x, y int) int {
+	return x ^ y
+}
+
+func main() {
+	pic.Show(Pic)
+}
